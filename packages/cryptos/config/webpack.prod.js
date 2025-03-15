3bfecf8c533bd075a200d5ef223382eb3a91d5f9
Marcelo Burriel <mburriel@santandertecnologia.com.ar>
2025-03-15 14:26:12 -0300

    Add container yml

---
+++ packages/cryptos/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "cryptos",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./CryptosApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
