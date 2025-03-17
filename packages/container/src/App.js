d51c0cb91eda13b019931a0d917c8b51dcc044e1
Marcelo Burriel <mburriel@santandertecnologia.com.ar>
2025-03-17 18:03:23 -0300

    Add automated invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import CryptosApp from "./components/CryptosApp";
 const App = () => {
   return (
     <div>
-      <h1>Container</h1>
+      <h1>Container!</h1>
       <hr />
       <CryptosApp />
     </div>
