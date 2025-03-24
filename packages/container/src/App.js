e2e7e8ada7bf58a77bbb34f9a2f63d99adfa05fa
Marcelo Burriel <mburriel@santandertecnologia.com.ar>
2025-03-24 19:13:22 -0300

    Config css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import CryptosApp from "./components/CryptosApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "container",
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <CryptosApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <CryptosApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/cryptos/src/App.js
+++ packages/cryptos/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 /* import ThemeProvider from "@flame-ui/utils/theme-provider";
 import businessTheme from "@flame-ui/themes/business"; */
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
-// Dummy change
+const generateClassName = createGenerateClassName({
+  productionPrefix: "cryptos",
+});
+
 export default () => {
   return (
     <div>
       {/* <ThemeProvider theme={businessTheme}> */}
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
